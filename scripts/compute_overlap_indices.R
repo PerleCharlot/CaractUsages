2d09ea508bcf89af1543903068661f62050732c4
Perle Charlot <perle.charlot@inrae.fr>
2023-03-16 17:09:54 +0100

    G_proba et E_proba

--- scripts/compute_overlap_indices.R
+++ scripts/compute_overlap_indices.R
@@ -2,13 +2,14 @@
 # Nom : Calcul indices overlap
 # Auteure : Perle Charlot
 # Date de création : 10-03-2023
-# Dates de modification : 15-03-2023
+# Dates de modification : 16-03-2023
 
 ### Librairies -------------------------------------
 library(data.table)
 library(dplyr)
 library(ggplot2)
 library(patchwork)
+library(raster)
 ### Fonctions -------------------------------------
 
 # Compute overlap D schoener between 2 uses
@@ -194,28 +195,98 @@ GridObs <- function(usage,  mois,
 }
 
 # Schoener D sur occupancy à partir données observations
-applyD_Schoener <- function(liste.usages, mois){
+applyD_Schoener_obs <- function(liste_usages, mois){
   
   # # TEST
   # liste.usages = c("Ni","Pa","Rp","Co","Vt")
   # mois = "juillet"
   
   # compute occupancy fpr each use
-  list_dt <- lapply(liste.usages, 
+  list_dt <- lapply(liste_usages, 
                     function(x) GridObs(usage = x, mois = mois))
   # sélectionner colonne p de chaque dt et index la (same as usage order)
   df = data.frame()
   for(i in 1:length(list_dt)){
     df.u = data.frame("p" = list_dt[[i]]$p)
-    names(df.u) = paste0("p_",liste.usages[i])
+    names(df.u) = paste0("p_",liste_usages[i])
     df = as.data.frame(append(df, df.u))
   }
   # Schoener D pairwise computation matrix
-  pairwise_D <- matrix(nrow = length(liste.usages), ncol = length(liste.usages), 
-                       dimnames = list(liste.usages,liste.usages) )
-  for(i in 1:length(liste.usages)){
-    for(j in 1:length(liste.usages)){
+  pairwise_D <- matrix(nrow = length(liste_usages), ncol = length(liste_usages), 
+                       dimnames = list(liste_usages,liste_usages) )
+  for(i in 1:length(liste_usages)){
+    for(j in 1:length(liste_usages)){
       pairwise_D[j,i]  <- DSchoener(df[,i], df[,j])
+    }
+  }
+  return(pairwise_D)
+}
+
+# Schoener D sur occupancy à partir données observations
+applyD_Schoener_proba_pred <- function(liste_usages, 
+                                       mois,
+                                       space){
+
+  # # TEST
+  # liste_usages = liste.usages
+  # mois = NULL # NULL "juin"
+  # space = "E" # "G"
+  
+  # sortir usage nom
+  noms_us_ord <- liste_usages[order(liste_usages)]
+  
+  if(space == "G"){
+    # load raster of proba for month studied
+    list_rast <- base::list.files(path = paste0(output_path,"/niches/", type_donnees,"/"),
+                                  pattern = ".tif$", 
+                                  recursive = T, full.names = T)
+    # trier le mois
+    list_rast <- list_rast[grep(mois,list_rast)]
+    #trier les usages
+    list_rast <- list_rast[grep(paste(liste_usages, collapse="|"), list_rast)]
+    # Load rasters
+    stack_us = stack(list_rast[grep(mois,list_rast)])
+    # rename with use names
+    names(stack_us) <- unlist(lapply(noms_us_ord, function(x) paste0(x,c("_obs","_proba","_pred"))))
+    # conserve proba layer = 2nd
+    stack_proba <- stack_us %>% subset(grep("proba", names(stack_us)))
+    # transform to df
+    df_proba_us <- data.frame(data.table(stack_proba[]))
+    
+    
+
+  }
+  if(space == "E"){
+    
+    # load raster of proba for month studied
+    list_rast <- base::list.files(path = paste0(output_path,"/niches/", type_donnees,"/"),
+                                  pattern = "niche_potentielle", 
+                                  recursive = T, full.names = T)
+    list_rast <- list_rast[grep(".rdata",list_rast)]
+    # fonction load niche data for a use
+    loadProbaNiche <- function(i){
+      load(list_rast[i])
+      names(grid_usage_rdata)[3] <- paste0(names(grid_usage_rdata)[3],"_",noms_us_ord[i])
+      return(grid_usage_rdata)
+    }
+    df_proba_us <- lapply(1:length(liste_usages), loadProbaNiche)
+    # Merge
+    df_proba_us <- Reduce(function(x, y) merge(x, y, all=FALSE), df_proba_us)
+    df_proba_us <- df_proba_us %>% subset(select=-grep("axe", names(df_proba_us)))
+  }
+
+  # rescale chaque proba (pour que ça fasse une distrib de proba => sum = 1)
+  df_proba_us_scale <- as.data.frame(apply(df_proba_us,
+                                           2,
+                                           function(x) x/sum(x, na.rm=T)))
+  #apply(df_proba_us_scale, 2, function(x) sum(x, na.rm=T))
+  
+  # Schoener D pairwise computation matrix
+  pairwise_D <- matrix(nrow = length(liste_usages), ncol = length(liste_usages), 
+                       dimnames = list(liste_usages,liste_usages) )
+  for(u1 in 1:length(liste_usages)){
+    for(u2 in 1:length(liste_usages)){
+      pairwise_D[u1,u2]  <- DSchoener(df_proba_us_scale[,u1], df_proba_us_scale[,u2])
     }
   }
   return(pairwise_D)
@@ -242,6 +313,96 @@ meansd4listMatrices <- function(liste.matrices, liste.usages){
   return(pairwise_smr)
 }
 
+
+schoenerD.stats <- function(fonction_applyschoener, chemin_save){
+  # # TEST
+  # fonction_applyschoener = "E_proba" # "E_obs" "E_proba"
+  # chemin_save = path_save
+  
+  if(fonction_applyschoener == "G_proba"){
+    A = lapply(liste.mois[-1], function(x) 
+      applyD_Schoener_proba_pred(liste_usages = c("Ni","Pa","Rp","Co","Vt"), mois = x, space="G") )
+    # Rp et VT présents tout l'été
+    Abis = lapply(liste.mois, function(x) 
+      applyD_Schoener_proba_pred(liste_usages = c("Rp","Vt"), mois = x) )
+    # Lk que en mai
+    B = applyD_Schoener_proba_pred(liste_usages = c("Lk","Rp","Vt"), mois = "mai")
+  }
+  if(fonction_applyschoener == "E_obs"){
+    A = lapply(liste.mois[-1], function(x) 
+      applyD_Schoener_obs(liste_usages = c("Ni","Pa","Rp","Co","Vt"), mois = x) )
+    # Rp et VT présents tous l'été
+    Abis = lapply(liste.mois, function(x) 
+      applyD_Schoener_obs(liste_usages = c("Rp","Vt"), mois = x) )
+    # Lk que en mai
+    B = applyD_Schoener_obs(liste_usages = c("Lk","Rp","Vt"), mois = "mai")
+  }
+  # pas de mean ni sd car niches construites sur aggrégation mois
+  if(fonction_applyschoener == "E_proba"){
+      D = applyD_Schoener_proba_pred(liste_usages = liste.usages, mois=NULL, space= "E")
+      D[upper.tri(D)] <- NA
+      diag(D) <- 1
+      df_D = as.data.frame(D)
+      write.csv(df_D, 
+                paste0(chemin_save,"/mat_schoener_d_summer.csv"))
+  }
+  
+  # quand D schoener mensuel, calcul mean + sd
+  if(any(fonction_applyschoener == "E_obs" | fonction_applyschoener == "G_proba")){
+    # Combine all 3
+    schoenerD_list = append(list(B),append(A, Abis))
+    names(schoenerD_list) <- c("mai", paste0(liste.mois[-1],"_5uses"),paste0(liste.mois,"_2uses"))
+    # save matrices
+    save(schoenerD_list,
+         file = paste0(chemin_save,"/matrices_schoener_d.rdata"))
+    # save in csv
+    for(i in 1:length(schoenerD_list)){
+      write.csv(schoenerD_list[i], 
+                paste0(chemin_save,"/matrice_schoener_d_",names(schoenerD_list)[i],".csv"))
+    }
+    # mean + sd throught summer
+    M1 = meansd4listMatrices(A, c("Ni","Pa","Rp","Co","Vt"))
+    names(M1) = paste0(names(M1),"_5uses")
+    M2 = meansd4listMatrices(Abis, c("Rp","Vt"))
+    names(M2) = paste0(names(M2),"_2uses")
+    M12 = append(M1, M2)
+    for(i in 1:length(M12)){
+      write.csv(M12[i], 
+                paste0(chemin_save,"/schoener_d_",names(M12)[i],".csv"))
+    }
+    M3 = B 
+    # Combiner à la main means & sd in the same matrix
+    M_mean = cbind(M1$mean,matrix(rep(NA,5), dimnames =list(c("Ni","Pa","Rp","Co","Vt"),"Lk")) )
+    N = matrix(c(NA,NA,M3[2,1],NA,M3[3,1],1), dimnames = list(c("Ni","Pa","Rp","Co","Vt","Lk"),"Lk"))
+    M_mean = rbind(M_mean, t(N))
+    # remplacer la valeur pour paire Rp/Vt
+    M_mean[5,3] <- M2$mean[2,1]
+    # NA dans triangle du haut
+    M_mean[upper.tri(M_mean)] <- NA
+    
+    M_sd = cbind(M1$sd,matrix(rep(NA,5), dimnames =list(c("Ni","Pa","Rp","Co","Vt"),"Lk")) )
+    Nsd = matrix(rep(NA,6), dimnames = list(c("Ni","Pa","Rp","Co","Vt","Lk"),"Lk"))
+    M_sd = rbind(M_sd, t(Nsd))
+    # remplacer la valeur pour paire Rp/Vt
+    M_sd[5,3] <- M2$sd[2,1]
+    # NA dans triangle du haut
+    M_sd[upper.tri(M_sd)] <- NA
+    # de la forme mean (+- sd) dans chaque cellule
+    M = matrix(paste0(round(M_mean, 2), " (", 
+                      round(M_sd, 2), ")"),
+               6,6,
+               dimnames = list(c("Ni","Pa","Rp","Co","Vt","Lk"),c("Ni","Pa","Rp","Co","Vt","Lk")))
+    M[upper.tri(M)] <- NA
+    diag(M) <- 1
+    M[6,c(1,2,4)] <- NA
+    df_mean_sd_D_obs = as.data.frame(M)
+    df_mean_sd_D_obs
+    write.csv(df_mean_sd_D_obs, 
+              paste0(chemin_save,"/mat_schoener_d_mean_sd_summer.csv"))
+  }
+  
+}
+
 ### Constantes -------------------------------------
 
 # Espace de travail
@@ -260,67 +421,52 @@ df.mois = data.frame(nom_mois = liste.mois, numero_mois = c("05","06","07","08",
 # Liste usages
 liste.usages = c("Ni","Lk","Co","Pa","Rp","Vt")
 
-### Programme -------------------------------------
-
-#### E-space ####
-
-##### Schoener D abond obs #####
-# corrigées par disponibilité (Broennimann 2012), env grid
-
-A = lapply(liste.mois[-1], function(x) 
-  applyD_Schoener(liste.usages = c("Ni","Pa","Rp","Co","Vt"), mois = x) )
-# Rp et VT présents tous l'été
-Abis = lapply(liste.mois, function(x) 
-  applyD_Schoener(liste.usages = c("Rp","Vt"), mois = x) )
-# Lk que en mai
-B = applyD_Schoener(liste.usages = c("Lk","Rp","Vt"), mois = "mai")
-schoenerD_list = append(list(B),A)
-names(schoenerD_list) <- c("mai", liste.mois[-1])
-# save matrices
 type_donnees = "ACP_avec_ponderation"
 algorithme = "glm"
 fit = "2_axes"
-schoener_D_obs_path = paste0(output_path,"/niches/",type_donnees,"/niche_overlap/",
-                            fit,"/",algorithme,"/Schoener_D_obs/") 
-if(!dir.exists(schoener_D_obs_path)){dir.create(schoener_D_obs_path)}
-# save in rdata
-save(schoenerD_list,
-     file = paste0(schoener_D_obs_path,"/matrices_schoener_d_obs.rdata"))
-# save in csv
-for(i in 1:length(schoenerD_list)){
-  write.csv(schoenerD_list[i], 
-            paste0(schoener_D_obs_path,"/matrice_schoener_d_obs_",names(schoenerD_list)[i],".csv"))
-}
-# mean + sd throught summer
-M1 = meansd4listMatrices(A, c("Ni","Pa","Rp","Co","Vt"))
-M2 = meansd4listMatrices(Abis, c("Rp","Vt"))
 
-M2[1,2]
+### Programme -------------------------------------
 
-M3 = B 
-M3[,2:3] <- NA
-M3
+#### E-space ####
+schoener_D_Espace_path = paste0(output_path,"/niches/",type_donnees,"/niche_overlap/",
+                                fit,"/",algorithme,"/Schoener_D/E_space") 
+if(!dir.exists(schoener_D_Espace_path)){dir.create(schoener_D_Espace_path, recursive = T)}
 
-# TODO : combine means & sd in the same matrix
-# de la forme mean (+- sd) dans chaque cellule
-
-
-
-
-
-
+##### Schoener D abond obs #####
+# corrigées par disponibilité (Broennimann 2012), env grid
+path_save <- paste0(schoener_D_Espace_path,"/obs/")
+if(!dir.exists(path_save)){dir.create(path_save, recursive = T)}
+schoenerD.stats("E_obs", path_save)
 
 #####  Schoener D sur probabilités issues SDM, projetées dans esp env ##### 
+path_save <- paste0(schoener_D_Espace_path,"/proba/")
+if(!dir.exists(path_save)){dir.create(path_save, recursive = T)}
+schoenerD.stats("E_proba", path_save)
 
 #####  % surface niche binarisé (en fonction cutoff) ##### 
 
-#### G-space ####
+path_save <- paste0(schoener_D_Espace_path,"/surface_niche/")
+if(!dir.exists(path_save)){dir.create(path_save, recursive = T)}
 
+#### G-space ####
+schoener_D_Gspace_path = paste0(output_path,"/niches/",type_donnees,"/niche_overlap/",
+                                fit,"/",algorithme,"/Schoener_D/G_space") 
+if(!dir.exists(schoener_D_Gspace_path)){dir.create(schoener_D_Gspace_path)}
 ##### % surface obs #####
 # ("témoin", ce qui est basiquement fait)
+path_save <- paste0(schoener_D_Gspace_path,"/surface_obs/")
+if(!dir.exists(path_save)){dir.create(path_save, recursive = T)}
+
+# monthly + stats
 
 ##### Schoener D proba occ ##### 
 # projetées dans esp géographique
+path_save <- paste0(schoener_D_Gspace_path,"/proba/")
+if(!dir.exists(path_save)){dir.create(path_save, recursive = T)}
+schoenerD.stats("G_proba", path_save)
 
 #####  % surface pred occ ##### 
 # (en fonction cutoff)
+
+path_save <- paste0(schoener_D_Gspace_path,"/surface_pred/")
+if(!dir.exists(path_save)){dir.create(path_save, recursive = T)}
